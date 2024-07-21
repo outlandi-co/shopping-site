d6d4de2737338082958faf7aae7b73ffdc747673
outlandi-co <outlandico@gmail.com>
2024-07-21 02:28:01 -0700

    created intro file, also added functionality for image overlay

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,10 @@
-// src/api/axios.js
 import axios from 'axios';
 
+// Use a default base URL if VITE_APP_API_BASE_URL is not defined
+const baseURL = import.meta.env.VITE_APP_API_BASE_URL || 'http://localhost:3000';
+
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: baseURL,
 });
 
 export default instance;
